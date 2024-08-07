5dbde4192f2950a2e3283d7fe863d7257e37315c
zebas1991 <sebastianmi798@outlook.com>
2024-08-07 12:31:51 -0600

    Update urls.py

--- webempresa/blog/urls.py
+++ webempresa/blog/urls.py
@@ -6,6 +6,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.BlogListView.as_view(), name= 'blog_list'),
-    path('<int:pk>/', views.BlogDetailView.as_view(), name='blog_detail'),
+    path('post/<int:pk>/', views.BlogDetailView.as_view(), name='blog_detail'),
     path('category/<int:category_id>/', views.category, name='category'),
 ]
